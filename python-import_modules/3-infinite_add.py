fd14712dc0e58d25b56d4dea6751bd8a90c4040e
nael21025 <elioussoufinael@gmail.com>
2026-01-17 14:48:40 +0100

    all

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-
-    total = 0
-
-    for arg in sys.argv[1:]:
-        total += int(arg)
-
-    print(total)
+    res = 0
+    for i in range(1, len(sys.argv)):
+        res += (int(sys.argv[i]))
+print(res)
---
+++ python-import_modules/README.md
@@ -0,0 +1,15 @@
+# Python Import Modules
+
+This directory contains exercises related to importing modules in Python.
+
+## Files
+
+- (Add your files here)
+
+## Usage
+
+Run the Python files using:
+
+```bash
+python filename.py
+```
