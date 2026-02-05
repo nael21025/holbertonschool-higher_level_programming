6e953729c0803842b58e35ce50d35d78572ae46c
nael21025 <elioussoufinael@gmail.com>
2026-02-05 02:54:24 +0100

    Fix: Remove forbidden words 'from' and 'import' in docstrings

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,14 +3,13 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if an object inherits from a class (but is not exactly that class).
+    """Check if object is instance of a subclass, but not exactly that class.
 
     Args:
         obj: The object to check
         a_class: The class to compare against
 
     Returns:
-        True if obj is an instance of a class that inherited from a_class,
-        False otherwise
+        True if obj is instance of subclass of a_class, False otherwise
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
