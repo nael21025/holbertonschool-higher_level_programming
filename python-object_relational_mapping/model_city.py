977786737f7d8075582c5d5ad6417ebcd59c93b9
nael21025 <elioussoufinael@gmail.com>
2026-03-06 15:10:53 +0100

    Fix model_city.py SQLAlchemy model syntax

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,32 +7,13 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
-# ...existing code...
-#!/usr/bin/python3
-"""Module that defines the City class for SQLAlchemy ORM model.
-The City class is linked to the MySQL table cities and has a
-foreign key relationship to the State class through state_id."""
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
-
 
 class City(Base):
-    """Class that defines the City model and links to the MySQL table
-    cities. Contains id as primary key, state_id as foreign key to states,
-    and name field. All fields have appropriate constraints."""
+    """Class that defines the City table."""
+
     __tablename__ = 'cities'
 
-    id = Column(
-        Integer,
-        primary_key=True,
-        autoincrement=True,
-        nullable=False
-    )
-    state_id = Column(
-        Integer,
-        ForeignKey('states.id'),
-        nullable=False
-    )
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
