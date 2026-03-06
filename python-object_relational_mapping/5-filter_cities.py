35e0ddf71226b9fa335a77b2d2b7dec1d214694c
nael21025 <elioussoufinael@gmail.com>
2026-03-06 13:50:11 +0100

    Fix MySQLdb connection: use port=3306 and charset=utf8 as per official example

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,11 +10,12 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        host="localhost",
-        charset="utf8mb4"
+        charset="utf8"
     )
     cursor = db.cursor()
     cursor.execute(
