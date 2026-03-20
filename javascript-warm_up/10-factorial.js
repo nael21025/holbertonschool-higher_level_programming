b20c606fc85c2ccbfd60af5fcbf65a90b6ec337e
nael21025 <elioussoufinael@gmail.com>
2026-03-20 10:00:10 +0100

    Add javascript-warm_up exercises 0-13

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  n = parseInt(n);
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(process.argv[2]));
